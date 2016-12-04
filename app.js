fd5cc8ea8598e2737f8a0cb72d9d01795dfc484e
kevin <kevinxpt27@gmail.com>
2016-12-03 23:08:06 -0500

    Cap 10

--- app.js
+++ app.js
@@ -11,10 +11,17 @@ var fs = require('fs');
 var appClientFiles = [
 'app_client/app.js',
 'app_client/home/home.controller.js',
+'app_client/about/about.controller.js',
+'app_client/locationDetail/locationDetail.controller.js',
+'app_client/reviewModal/reviewModal.controller.js',
 'app_client/common/services/geolocation.service.js',
 'app_client/common/services/loc8rData.service.js',
 'app_client/common/filters/formatDistance.filter.js',
-'app_client/common/directives/ratingStars/ratingStars.directive.js'
+'app_client/common/filters/addHtmlLineBreaks.filter.js',
+'app_client/common/directives/ratingStars/ratingStars.directive.js',
+'app_client/common/directives/footerGeneric/footerGeneric.directive.js',
+'app_client/common/directives/navigation/navigation.directive.js',
+'app_client/common/directives/pageHeader/pageHeader.directive.js'
 ];
 
 var uglified = uglifyJs.minify(appClientFiles, { compress : false });
@@ -46,9 +53,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app_client')));//PARA AGREGAR EL ANGULAR
 
-app.use('/', routes);
+//app.use('/', routes);
 app.use('/api', routesApi);//ESPECIFICA QUE SE USARA /API PARA LAS PETICIONES GET POST DELETE
-app.use('/users', users);
+//app.use('/users', users);
+
+app.use(function(req, res) {
+  res.sendfile(path.join(__dirname, 'app_client', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app_client/common/services/loc8rData.service.js
+++ app_client/common/services/loc8rData.service.js
@@ -9,8 +9,18 @@ function loc8rData ($http) {
     var locationByCoords = function (lat, lng) {
         return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&maxDistance=1000000000');
     };
+    //Para Obtener el detalle de Location
+    var locationById = function (locationid) {
+        return $http.get('/api/locations/' + locationid);
+    };
+    //servicio para insertar reviews
+    var addReviewById = function (locationid, data) {
+        return $http.post('/api/locations/' + locationid + '/reviews', data);
+    };
     return {
-        locationByCoords : locationByCoords
+        locationByCoords : locationByCoords,
+        locationById : locationById,
+        addReviewById : addReviewById
     };
 };
 })();
